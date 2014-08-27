55b0ab2fbe49c5df4ffc4a4f50f12358fd0f248b
Howard <how2945ard@gmail.com>
2014-08-28 02:14:56 +0800

    integer

--- db/migrate/20140824160841_add_detail_to_enrolls.rb
+++ db/migrate/20140824160841_add_detail_to_enrolls.rb
@@ -3,13 +3,13 @@ class AddDetailToEnrolls < ActiveRecord::Migration
 		add_column :enrolls, :name, :string
 		add_column :enrolls, :school, :string
 		add_column :enrolls, :major, :string
-		add_column :enrolls, :year, :number
+		add_column :enrolls, :year, :integer
 		add_column :enrolls, :cellphone, :string
 		add_column :enrolls, :email, :string
 		add_column :enrolls, :image, :string
 		add_column :enrolls, :videoUrl, :string
 		add_column :enrolls, :videoInfo, :string
 		add_column :enrolls, :topic, :string
-		add_column :enrolls, :view, :number
+		add_column :enrolls, :view, :integer
 	end
 end
