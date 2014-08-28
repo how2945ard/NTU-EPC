0c4699dc3f729f9c9f551603895f26075cb6e38b
Howard <how2945ard@gmail.com>
2014-08-28 13:57:26 +0800

    fb linked

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Ntu::Application.routes.draw do
   get '/' => 'home#index'
   get '/get_enroll' => 'home#get_enroll'
   get '/enrolls' => 'home#show'
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140824160841) do
+ActiveRecord::Schema.define(:version => 20140828044953) do
 
   create_table "enrolls", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(:version => 20140824160841) do
     t.string   "videoInfo"
     t.string   "topic"
     t.integer  "view"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
 end
