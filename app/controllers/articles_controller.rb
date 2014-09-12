8194e91f753b91e21921a207d01c37dd46ff0330
Howard <how2945ard@gmail.com>
2014-09-13 02:47:13 +0800

    fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with :name => "admin", :password => "admin"
+  http_basic_authenticate_with :name => "admin", :password => "admin", except: [:show]
   # GET /articles
   # GET /articles.json
   def index
